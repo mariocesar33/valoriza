67655d697921efabf2a6399a9477c7a91ecd0d29
Mário César Silva <mcgato33@hotmail.com>
2021-06-25 06:36:51 -0100

    import database

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send("Ola Mundo!")
-})
+import "./database";
 
-app.post("/", (request, response) => {
-  return response.json({ message: "Ola json!" })
-})
 
 app.listen(3333, () => console.log("Servidor ligado."));
